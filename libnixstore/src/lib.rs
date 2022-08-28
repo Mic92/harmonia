5a8f67b175b0de0fe1487f8d7bdc4c9f636407e2
Simon Hauser <Simon-Hauser@outlook.de>
2022-08-28 10:57:35 +0200

    refactor(libnixstore): error handling and must_use

--- libnixstore/src/lib.rs
+++ libnixstore/src/lib.rs
@@ -8,28 +8,9 @@
 )]
 #![forbid(non_ascii_idents)]
 
-#[derive(Debug)]
-pub enum NixErr {
-    Exception(String),
-    Receiver(tokio::sync::oneshot::error::RecvError),
-}
+mod error;
 
-impl std::fmt::Display for NixErr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            NixErr::Receiver(e) => write!(f, "{}", e),
-            NixErr::Exception(e) => write!(f, "{}", e),
-        }
-    }
-}
-
-impl std::error::Error for NixErr {}
-
-impl From<cxx::Exception> for NixErr {
-    fn from(err: cxx::Exception) -> Self {
-        NixErr::Exception(err.what().to_owned())
-    }
-}
+use crate::error::NixErr;
 
 pub enum ReferencesRet {
     Ok(Vec<String>),
@@ -165,10 +146,11 @@ pub fn init() {
 #[inline]
 /// Set the loglevel.
 pub fn set_verbosity(level: i32) {
-    ffi::set_verbosity(level)
+    ffi::set_verbosity(level);
 }
 
 #[inline]
+#[must_use]
 /// Check whether a path is valid.
 pub fn is_valid_path(path: &str) -> bool {
     ffi::is_valid_path(path).unwrap_or(false)
@@ -190,11 +172,12 @@ pub async fn query_references(path: &str) -> Result<Vec<String>, NixErr> {
 
 #[inline]
 /// Return narhash of a valid path. It is permitted to omit the name part of the store path.
-pub fn query_path_hash(path: &str) -> Result<String, cxx::Exception> {
-    ffi::query_path_hash(path)
+pub fn query_path_hash(path: &str) -> Result<String, NixErr> {
+    Ok(ffi::query_path_hash(path)?)
 }
 
 #[inline]
+#[must_use]
 /// Return deriver of a valid path. It is permitted to omit the name part of the store path.
 pub fn query_deriver(path: &str) -> Option<String> {
     match ffi::query_deriver(path) {
@@ -205,7 +188,7 @@ pub fn query_deriver(path: &str) -> Option<String> {
 
 #[inline]
 /// Query information about a valid path. It is permitted to omit the name part of the store path.
-pub fn query_path_info(path: &str, base32: bool) -> Result<PathInfo, cxx::Exception> {
+pub fn query_path_info(path: &str, base32: bool) -> Result<PathInfo, NixErr> {
     let res = ffi::query_path_info(path, base32)?;
     Ok(PathInfo {
         drv: string_to_opt(res.drv),
@@ -219,6 +202,7 @@ pub fn query_path_info(path: &str, base32: bool) -> Result<PathInfo, cxx::Except
 }
 
 #[inline]
+#[must_use]
 /// Query the information about a realisation
 pub fn query_raw_realisation(output_id: &str) -> Option<String> {
     match ffi::query_raw_realisation(output_id) {
@@ -228,6 +212,7 @@ pub fn query_raw_realisation(output_id: &str) -> Option<String> {
 }
 
 #[inline]
+#[must_use]
 /// Query the full store path given the hash part of a valid store path, or empty if the path
 /// doesn't exist.
 pub fn query_path_from_hash_part(hash_part: &str) -> Option<String> {
@@ -247,83 +232,87 @@ pub fn compute_fs_closure(
     flip_direction: bool,
     include_outputs: bool,
     paths: Vec<&str>,
-) -> Result<Vec<String>, cxx::Exception> {
-    ffi::compute_fs_closure(flip_direction, include_outputs, paths)
+) -> Result<Vec<String>, NixErr> {
+    Ok(ffi::compute_fs_closure(
+        flip_direction,
+        include_outputs,
+        paths,
+    )?)
 }
 
 #[inline]
 /// Sort a set of paths topologically under the references relation. If `p` refers to `q`, then `p`
 /// precedes `q` in this list.
-pub fn topo_sort_paths(paths: Vec<&str>) -> Result<Vec<String>, cxx::Exception> {
-    ffi::topo_sort_paths(paths)
+pub fn topo_sort_paths(paths: Vec<&str>) -> Result<Vec<String>, NixErr> {
+    Ok(ffi::topo_sort_paths(paths)?)
 }
 
 #[inline]
 /// Follow symlinks until we end up with a path in the Nix store. Will transform the results to
 /// store paths.
-pub fn follow_links_to_store_path(path: &str) -> Result<String, cxx::Exception> {
-    ffi::follow_links_to_store_path(path)
+pub fn follow_links_to_store_path(path: &str) -> Result<String, NixErr> {
+    Ok(ffi::follow_links_to_store_path(path)?)
 }
 
 #[inline]
 /// Export multiple paths in the format expected by `nix-store --import`.
-pub fn export_paths(fd: i32, paths: Vec<&str>) -> Result<(), cxx::Exception> {
-    ffi::export_paths(fd, paths)
+pub fn export_paths(fd: i32, paths: Vec<&str>) -> Result<(), NixErr> {
+    Ok(ffi::export_paths(fd, paths)?)
 }
 
 #[inline]
 /// Import a sequence of NAR dumps created by `export_paths()` into the Nix store. Optionally, the
 /// contents of the NARs are preloaded into the specified FS accessor to speed up subsequent
 /// access.
-pub fn import_paths(fd: i32, dont_check_signs: bool) -> Result<(), cxx::Exception> {
-    ffi::import_paths(fd, dont_check_signs)
+pub fn import_paths(fd: i32, dont_check_signs: bool) -> Result<(), NixErr> {
+    Ok(ffi::import_paths(fd, dont_check_signs)?)
 }
 
 #[inline]
 /// Compute the hash of the given path. The hash is defined as (essentially)
 /// `hashString(ht, dumpPath(path))`.
-pub fn hash_path(algo: &str, base32: bool, path: &str) -> Result<String, cxx::Exception> {
-    ffi::hash_path(algo, base32, path)
+pub fn hash_path(algo: &str, base32: bool, path: &str) -> Result<String, NixErr> {
+    Ok(ffi::hash_path(algo, base32, path)?)
 }
 
 #[inline]
 /// Compute the hash of the given file.
-pub fn hash_file(algo: &str, base32: bool, path: &str) -> Result<String, cxx::Exception> {
-    ffi::hash_file(algo, base32, path)
+pub fn hash_file(algo: &str, base32: bool, path: &str) -> Result<String, NixErr> {
+    Ok(ffi::hash_file(algo, base32, path)?)
 }
 
 #[inline]
 /// Compute the hash of the given string.
-pub fn hash_string(algo: &str, base32: bool, s: &str) -> Result<String, cxx::Exception> {
-    ffi::hash_string(algo, base32, s)
+pub fn hash_string(algo: &str, base32: bool, s: &str) -> Result<String, NixErr> {
+    Ok(ffi::hash_string(algo, base32, s)?)
 }
 
 #[inline]
 /// Parse the hash from a string representation in the format `[<type>:]<base16|base32|base64>` or
 /// `<type>-<base64>` to a string representation of the hash, in `base-16`, `base-32`. The result
 /// is not prefixed by the hash type.
-pub fn convert_hash(algo: &str, s: &str, to_base_32: bool) -> Result<String, cxx::Exception> {
-    ffi::convert_hash(algo, s, to_base_32)
+pub fn convert_hash(algo: &str, s: &str, to_base_32: bool) -> Result<String, NixErr> {
+    Ok(ffi::convert_hash(algo, s, to_base_32)?)
 }
 
 #[inline]
 /// Return a detached signature of the given string.
-pub fn sign_string(secret_key: &str, msg: &str) -> Result<String, cxx::Exception> {
-    ffi::sign_string(secret_key, msg)
+pub fn sign_string(secret_key: &str, msg: &str) -> Result<String, NixErr> {
+    Ok(ffi::sign_string(secret_key, msg)?)
 }
 
 #[inline]
 /// Verify that `sig` is a valid signature for `msg`, using the signer's `public_key`.
-pub fn check_signature(public_key: &str, sig: &str, msg: &str) -> Result<bool, cxx::Exception> {
-    ffi::check_signature(public_key, sig, msg)
+pub fn check_signature(public_key: &str, sig: &str, msg: &str) -> Result<bool, NixErr> {
+    Ok(ffi::check_signature(public_key, sig, msg)?)
 }
 
 #[inline]
 /// This is the preparatory part of `addToStore()`;
 ///
 /// It computes the store path to which `src_path` is to be copied. Returns the store path.
-pub fn add_to_store(src_path: &str, recursive: i32, algo: &str) -> Result<String, cxx::Exception> {
-    ffi::add_to_store(src_path, recursive, algo)
+pub fn add_to_store(src_path: &str, recursive: i32, algo: &str) -> Result<String, NixErr> {
+    Ok(ffi::add_to_store(src_path, recursive, algo)?)
 }
 
 #[inline]
@@ -332,13 +321,13 @@ pub fn make_fixed_output_path(
     algo: &str,
     hash: &str,
     name: &str,
-) -> Result<String, cxx::Exception> {
-    ffi::make_fixed_output_path(recursive, algo, hash, name)
+) -> Result<String, NixErr> {
+    Ok(ffi::make_fixed_output_path(recursive, algo, hash, name)?)
 }
 
 #[inline]
 /// Read a derivation, after ensuring its existence through `ensurePath()`.
-pub fn derivation_from_path(drv_path: &str) -> Result<Drv, cxx::Exception> {
+pub fn derivation_from_path(drv_path: &str) -> Result<Drv, NixErr> {
     let res = ffi::derivation_from_path(drv_path)?;
     let mut outputs = std::collections::HashMap::new();
     for out in res.outputs {
@@ -364,23 +353,26 @@ pub fn derivation_from_path(drv_path: &str) -> Result<Drv, cxx::Exception> {
 #[inline]
 /// Add a store path as a temporary root of the garbage collector. The root disappears as soon as
 /// we exit.
-pub fn add_temp_root(store_path: &str) -> Result<(), cxx::Exception> {
-    ffi::add_temp_root(store_path)
+pub fn add_temp_root(store_path: &str) -> Result<(), NixErr> {
+    Ok(ffi::add_temp_root(store_path)?)
 }
 
 #[inline]
+#[must_use]
 /// Return the path to the directory where the main programs are stored.
 pub fn get_bin_dir() -> String {
     ffi::get_bin_dir()
 }
 
 #[inline]
+#[must_use]
 /// Returns the path to the directory where nix store sources and derived files.
 pub fn get_store_dir() -> String {
     ffi::get_store_dir()
 }
 
 #[inline]
+#[must_use]
 /// Return the build log of the specified store path, if available, or null otherwise.
 pub fn get_build_log(derivation_path: &str) -> Option<String> {
     match ffi::get_build_log(derivation_path) {
@@ -391,8 +383,8 @@ pub fn get_build_log(derivation_path: &str) -> Option<String> {
 
 #[inline]
 /// Return a JSON representation as String of the contents of a NAR (except file contents).
-pub fn get_nar_list(store_path: &str) -> Result<String, cxx::Exception> {
-    ffi::get_nar_list(store_path)
+pub fn get_nar_list(store_path: &str) -> Result<String, NixErr> {
+    Ok(ffi::get_nar_list(store_path)?)
 }
 
 #[cfg(test)]
